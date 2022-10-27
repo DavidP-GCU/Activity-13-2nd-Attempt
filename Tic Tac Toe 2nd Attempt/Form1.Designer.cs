368729b75a98d00da5e83251fc929c17914d0d93
DavidP-GCU <114124010+DavidP-GCU@users.noreply.github.com>
2022-10-27 17:50:43 -0600

    Add files via upload

---
+++ Tic Tac Toe 2nd Attempt/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Tic_Tac_Toe_2nd_Attempt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.nEWGAMEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iNFOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nEWGAMEToolStripMenuItem,
+            this.iNFOToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(684, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // nEWGAMEToolStripMenuItem
+            // 
+            this.nEWGAMEToolStripMenuItem.Name = "nEWGAMEToolStripMenuItem";
+            this.nEWGAMEToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.nEWGAMEToolStripMenuItem.Text = "NEW GAME";
+            this.nEWGAMEToolStripMenuItem.Click += new System.EventHandler(this.nEWGAMEToolStripMenuItem_Click);
+            // 
+            // iNFOToolStripMenuItem
+            // 
+            this.iNFOToolStripMenuItem.Name = "iNFOToolStripMenuItem";
+            this.iNFOToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.iNFOToolStripMenuItem.Text = "INFO";
+            this.iNFOToolStripMenuItem.Click += new System.EventHandler(this.iNFOToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.btn9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(684, 637);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // btn1
+            // 
+            this.btn1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn1.FlatAppearance.BorderSize = 5;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.Font = new System.Drawing.Font("Lucida Console", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(3, 3);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(222, 206);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn2.FlatAppearance.BorderSize = 5;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.Font = new System.Drawing.Font("Lucida Console", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(231, 3);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(222, 206);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn3.FlatAppearance.BorderSize = 5;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3.Font = new System.Drawing.Font("Lucida Console", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(459, 3);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(222, 206);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn4.FlatAppearance.BorderSize = 5;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn4.Font = new System.Drawing.Font("Lucida Console", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(3, 215);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(222, 206);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn5.FlatAppearance.BorderSize = 5;
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn5.Font = new System.Drawing.Font("Lucida Console", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(231, 215);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(222, 206);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn6.FlatAppearance.BorderSize = 5;
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn6.Font = new System.Drawing.Font("Lucida Console", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(459, 215);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(222, 206);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn7.FlatAppearance.BorderSize = 5;
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn7.Font = new System.Drawing.Font("Lucida Console", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(3, 427);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(222, 207);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn8.FlatAppearance.BorderSize = 5;
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn8.Font = new System.Drawing.Font("Lucida Console", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(231, 427);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(222, 207);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn9.FlatAppearance.BorderSize = 5;
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn9.Font = new System.Drawing.Font("Lucida Console", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(459, 427);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(222, 207);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(684, 661);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.MinimumSize = new System.Drawing.Size(350, 500);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe Game";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nEWGAMEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iNFOToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ Tic Tac Toe 2nd Attempt/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe_2nd_Attempt
+{
+    public partial class Form1 : Form
+    {
+        //Create Variables
+        char who = 'O';
+        short movement = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void nEWGAMEToolStripMenuItem_Click(object sender, EventArgs e) //New Game menu event handler. 
+        {
+            who = 'O';
+            movement = 0;
+            btn1.Enabled = true; btn1.Text = "";
+            btn2.Enabled = true; btn2.Text = "";
+            btn3.Enabled = true; btn3.Text = "";
+            btn4.Enabled = true; btn4.Text = "";
+            btn5.Enabled = true; btn5.Text = "";
+            btn6.Enabled = true; btn6.Text = "";
+            btn7.Enabled = true; btn7.Text = "";
+            btn8.Enabled = true; btn8.Text = "";
+            btn9.Enabled = true; btn9.Text = "";
+            tableLayoutPanel1.Enabled = true;
+        }
+
+        private void iNFOToolStripMenuItem_Click(object sender, EventArgs e)//Info Event handler
+        {
+            MessageBox.Show("A simple game of Tic Tac Toe.");
+        }
+
+        private void btn1_Click(object sender, EventArgs e)//Button event handler. Replicated across all 9 buttons. 
+        {
+            Button bt = sender as Button;
+            bt.Enabled = false;
+            bt.BackColor = Color.Gray; //Box color after user clicks on button. 
+            if(who == 'O')
+            {
+                //Create if else if loop
+                bt.Text = "O";
+                if((btn1.Text == btn2.Text && btn2.Text == btn3.Text && btn2.Text != "") ||
+                    (btn4.Text == btn5.Text && btn5.Text == btn6.Text && btn5.Text != "") ||
+                    (btn7.Text == btn8.Text && btn8.Text == btn9.Text && btn8.Text != "") ||
+                    (btn1.Text == btn4.Text && btn4.Text == btn7.Text && btn4.Text != "") ||
+                    (btn2.Text == btn5.Text && btn5.Text == btn8.Text && btn5.Text != "") ||
+                    (btn3.Text == btn6.Text && btn6.Text == btn9.Text && btn6.Text != "") ||
+                    (btn1.Text == btn5.Text && btn5.Text == btn9.Text && btn5.Text != "") ||
+                    (btn3.Text == btn5.Text && btn5.Text == btn7.Text && btn5.Text != ""))
+                {
+                    MessageBox.Show(($"The Winner is player{who.ToString().ToUpper()} !!!"));
+                    tableLayoutPanel1.Enabled = false;
+                }
+                else if(movement == 8)
+                {
+                    //If game ends in draw. Display message informing user of draw. 
+                    MessageBox.Show("This game has ended in a draw.");
+                }
+
+                who = 'X';
+            }
+            else if(who == 'X')
+            {
+                bt.Text = "X";
+                
+                //Create if else if loop. 
+                if ((btn1.Text == btn2.Text && btn2.Text == btn3.Text && btn2.Text != "") ||
+                   (btn4.Text == btn5.Text && btn5.Text == btn6.Text && btn5.Text != "") ||
+                   (btn7.Text == btn8.Text && btn8.Text == btn9.Text && btn8.Text != "") ||
+                   (btn1.Text == btn4.Text && btn4.Text == btn7.Text && btn4.Text != "") ||
+                   (btn2.Text == btn5.Text && btn5.Text == btn8.Text && btn5.Text != "") ||
+                   (btn3.Text == btn6.Text && btn6.Text == btn9.Text && btn6.Text != "") ||
+                   (btn1.Text == btn5.Text && btn5.Text == btn9.Text && btn5.Text != "") ||
+                   (btn3.Text == btn5.Text && btn5.Text == btn7.Text && btn5.Text != ""))
+                {
+                    //If there is a winner. Display message. 
+                    MessageBox.Show(($"Congratulations {who.ToString().ToUpper()} !!!"));
+                    tableLayoutPanel1.Enabled = false;
+                }
+                else if (movement == 8)
+                {
+                    //Display error message. 
+                    MessageBox.Show("ERROR");
+                }
+                who = 'O';
+            }
+            movement++;//Increment by 1 
+        }
+    }
+}
